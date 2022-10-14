196943998b6733ffb6be44eb1927603fe43fe759
Alina <alina.panova.2014@mail.ru>
2022-10-14 21:40:46 +0300

    Доделана практика с циклами

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ for (let i = 0; i < 2; i++) {
   lastFilm = "";
   rating = "";
 }
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
